407ae8ac4e9f4abe28b2d4d714cb00f1b228c3e0
sbpak1 <sbpak1@gmail.com>
2025-09-24 10:33:37 +0900

    기본 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@':path.resolve(__dirname,'src')
+    }
+  },
+  server:{
+    port: 3000,
+    proxy: {
+      // 프론트에서 /api 로 호출하면 -> http://localhost:8080/Api
+      '/api':{
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      }
+    }
+  }
 })
